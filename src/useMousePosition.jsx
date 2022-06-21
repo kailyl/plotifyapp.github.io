e81f6a64d7848e83dc8f6b8a5a5cec4872ebd1d7
kailyl <kliu279@gmail.com>
2022-06-20 22:22:22 -0400

    for ~mobile~ (i hope)

--- src/useMousePosition.jsx
+++ src/useMousePosition.jsx
@@ -1,16 +1,28 @@
 import { useEffect, useState } from "react";
+import {isMobile} from 'react-device-detect';
 
 export function useMousePosition() {
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
-  useEffect(() => {
-    const setFromEvent = (e) => setPosition({ x: e.clientX, y: e.clientY });
-    window.addEventListener("mousemove", setFromEvent);
-
-    return () => {
-      window.removeEventListener("mousemove", setFromEvent);
-    };
-  }, []);
-
+  if(isMobile) {
+    useEffect(() => {
+      const setFromEvent = (e) => setPosition({ x: e.touches[0].clientX, y: e.touches[0].clientY });
+      window.addEventListener("touchmove", setFromEvent);
+  
+      return () => {
+        window.removeEventListener("touchmove", setFromEvent);
+      };
+    }, []);
+  } else {
+    useEffect(() => {
+      const setFromEvent = (e) => setPosition({ x: e.clientX, y: e.clientY });
+      window.addEventListener("mousemove", setFromEvent);
+  
+      return () => {
+        window.removeEventListener("mousemove", setFromEvent);
+      };
+    }, []);
+  }
+  
   return position;
 };
