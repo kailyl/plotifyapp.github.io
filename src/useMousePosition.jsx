d379677fdd663166ddadef98c81bdcec805adaaa
kailyl <kliu279@gmail.com>
2022-06-20 23:12:42 -0400

    pls work touches

--- src/useMousePosition.jsx
+++ src/useMousePosition.jsx
@@ -4,29 +4,29 @@ import {isMobile} from 'react-device-detect';
 export function useMousePosition() {
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
-  if(isMobile) {
+  // if(isMobile) {
     useEffect(() => {
-      const setFromEvent = (e) => setPosition({ x: e.touches[0].clientX, y: e.touches[0].clientY });
-      window.addEventListener("touchmove", setFromEvent);
+      const setFromEvent = (e) => {
+        console.log("HELLO")
+        console.log(e.touches[0].clientX)
+        setPosition({ x: e.touches[0].clientX, y: e.touches[0].clientY });
+      }
       window.addEventListener("touchstart", setFromEvent);
-      window.addEventListener("touchend", setFromEvent);
   
       return () => {
         window.removeEventListener("touchstart", setFromEvent);
-        window.removeEventListener("touchmove", setFromEvent);
-        window.removeEventListener("touchend", setFromEvent);
       };
     }, []);
-  } else {
-    useEffect(() => {
-      const setFromEvent = (e) => setPosition({ x: e.clientX, y: e.clientY });
-      window.addEventListener("mousemove", setFromEvent);
+  // } else {
+  //   useEffect(() => {
+  //     const setFromEvent = (e) => setPosition({ x: e.clientX, y: e.clientY });
+  //     window.addEventListener("mousemove", setFromEvent);
   
-      return () => {
-        window.removeEventListener("mousemove", setFromEvent);
-      };
-    }, []);
-  }
+  //     return () => {
+  //       window.removeEventListener("mousemove", setFromEvent);
+  //     };
+  //   }, []);
+  // }
   
   return position;
 };
